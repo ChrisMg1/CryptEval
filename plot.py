74ec671f0bbcb599234cdfaec64132fbd8851fde
ChrisMg1_local_home <christoph.maget@outlook.com>
2020-10-03 17:11:42 +0200

    bw and grid

--- plot.py
+++ plot.py
@@ -24,9 +24,9 @@ print(AnzahlKonst)
 
 fig1 = plt.figure(1)
 ax1 = fig1.add_subplot(111)
-ax1.plot(LaengeKonst['AnzahlVerschluesselungen'], LaengeKonst['ZeitAES'], color='g', marker='o', markersize=1.5,
+ax1.plot(LaengeKonst['AnzahlVerschluesselungen'], LaengeKonst['ZeitAES'], color='k', marker='o', markersize=1.5,
          linewidth=0.1, label=str('AES-256'))
-ax1.plot(LaengeKonst['AnzahlVerschluesselungen'], LaengeKonst['ZeitOTP'], color='r', marker='s', markersize=1.5,
+ax1.plot(LaengeKonst['AnzahlVerschluesselungen'], LaengeKonst['ZeitOTP'], color='0.7', marker='s', markersize=1.5,
          linewidth=0.1, label=str('SIKAF (OTP)'))
 
 if lang_plot == 'eng':
@@ -36,6 +36,7 @@ if lang_plot == 'eng':
     ax1.set_ylim(ymin=0, ymax=800)
     ax1.set_xlim(xmin=0)
     plt.legend()
+    # plt.grid()
     # fig1.savefig('C:/Users/blue/Documents/Forschungsprojekt-FUH/Ausarbeitung/Abbildungen/plots/AnzahlVStime_english.svg',
     fig1.savefig('AnzahlVStime_english.svg',
                  format='svg', orientation='landscape', pad_inches=0, dpi=1200)
@@ -47,6 +48,7 @@ else:
     ax1.set_ylim(ymin=0, ymax=800)
     ax1.set_xlim(xmin=0)
     plt.legend()
+    # plt.grid()
     # fig1.savefig('C:/Users/blue/Documents/Forschungsprojekt-FUH/Ausarbeitung/Abbildungen/plots/AnzahlVStime.pdf',
     fig1.savefig('AnzahlVStime.pdf',
                  format='pdf', orientation='landscape', pad_inches=0)
@@ -55,9 +57,9 @@ else:
 
 fig2 = plt.figure(2)
 ax2 = fig2.add_subplot(111)
-ax2.plot(AnzahlKonst['Nachrichtenlaenge'], AnzahlKonst['ZeitAES'], color='g', marker='o', markersize=0.5,
+ax2.plot(AnzahlKonst['Nachrichtenlaenge'], AnzahlKonst['ZeitAES'], color='k', marker='o', markersize=0.5,
          linewidth = 0.1, label=str('AES-256'))
-ax2.plot(AnzahlKonst['Nachrichtenlaenge'], AnzahlKonst['ZeitOTP'], color='r', marker='s', markersize=0.5,
+ax2.plot(AnzahlKonst['Nachrichtenlaenge'], AnzahlKonst['ZeitOTP'], color='0.7', marker='s', markersize=0.5,
          linewidth = 0.1, label=str('SIKAF (OTP)'))
 
 if lang_plot == 'eng':
@@ -72,7 +74,7 @@ if lang_plot == 'eng':
                  format='svg', orientation='landscape', pad_inches=0, dpi=1200)
 
 else:
-    ax2.set_xlabel('Nachrichtenlänge [byte]')
+    ax2.set_xlabel('Nachrichtenlänge [Byte]')
     ax2.set_ylabel('Zeit [ms]')
     ax2.set_title(str('Anzahl Maskierungen konstant bei n=' + str(my_parameters.AnzahlVerschluesselungen_iter)))
     ax2.set_ylim(ymin=0, ymax=800)
